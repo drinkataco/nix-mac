c21e9a26fda0c14f459982f902127ffe7f8a9539
J J Walwyn <git@jo.shw.al>
2026-03-13 22:34:07 +0000

    Add standard packages

--- modules/darwin/apps/nixpkgs.nix
+++ modules/darwin/apps/nixpkgs.nix
@@ -1,7 +1,52 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    mkvtoolnix
+    # Shell and terminal
+    bash
+    bat
+    cmatrix
+    neovim
+    tmux
+    tree
+    watch
+    zoxide
+    zsh
+    zsh-completions
+    zsh-syntax-highlighting
+
+    # Core Unix tools
+    coreutils
+    gawk
+    gettext
+    gnugrep
+    gnused
+    gnutar
+
+    # Dev tooling
+    ansible
+    awscli2
+    delta
+    fnm
+    gcc
+    gnupg
+    go
+    jq
+    k9s
+    lazygit
+    nmap
+    opentofu
+    openvpn
+    pinentry_mac
+    shellcheck
+    shfmt
+    silver-searcher
+
+    # Monitoring and networking
+    htop
     wireshark
+
+    # Media and data tools
+    mkvtoolnix
+    xan
   ];
 }
