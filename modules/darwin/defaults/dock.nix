2629bd2280416dc30586c69bd2cb8d49faba45b7
J J Walwyn <git@jo.shw.al>
2026-03-13 10:37:26 +0000

    Configure Dock

--- modules/darwin/defaults/dock.nix
+++ modules/darwin/defaults/dock.nix
@@ -1,8 +1,39 @@
+{ username, ... }:
 {
   system.defaults.dock = {
     autohide = true;
+    magnification = false;
+    minimize-to-application = true;
     mru-spaces = false;
+    persistent-apps = [
+      {
+        app = "/System/Library/CoreServices/Finder.app";
+      }
+      {
+        spacer = {
+          small = false;
+        };
+      }
+    ];
+    persistent-others = [
+      {
+        folder = {
+          arrangement = "date-added";
+          displayas = "folder";
+          path = "/Users/${username}/Downloads";
+          showas = "grid";
+        };
+      }
+      {
+        folder = {
+          arrangement = "date-added";
+          displayas = "folder";
+          path = "/Applications";
+          showas = "grid";
+        };
+      }
+    ];
     show-recents = true;
-    tilesize = 40;
+    tilesize = 48;
   };
 }
