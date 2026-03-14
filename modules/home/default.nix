2d814e93e1e5101bda118d8f55dfa02083050421
J J Walwyn <git@jo.shw.al>
2026-03-14 11:57:02 +0000

    Set explicit Home Manager home path

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,9 +1,12 @@
+{ username, ... }:
 {
   imports = [
     ./dotfiles.nix
     ./ssh.nix
   ];
 
+  home.username = username;
+  home.homeDirectory = "/Users/${username}";
   home.stateVersion = "25.05";
 
   programs.home-manager.enable = true;
