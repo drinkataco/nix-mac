c3a06d86db02f72e7fef0947261ee14d46786e8f
J J Walwyn <git@jo.shw.al>
2026-03-14 17:18:06 +0000

    Simplify Alacritty config and sync dotfiles tree

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -1,4 +1,22 @@
-{
-  home.file.".config/alacritty/alacritty.toml".source =
-    ../../dotfiles/.config/alacritty/alacritty.toml;
+{ lib, ... }:
+let
+  dotfilesDir = ../../dotfiles;
+  mkDotfileEntry = name: type:
+    if type == "directory" then {
+      name = name;
+      value = {
+        source = dotfilesDir + "/${name}";
+        recursive = true;
+      };
+    } else if type == "regular" || type == "symlink" then {
+      name = name;
+      value.source = dotfilesDir + "/${name}";
+    } else
+      null;
+in {
+  home.file = builtins.listToAttrs (
+    builtins.filter (entry: entry != null) (
+      lib.mapAttrsToList mkDotfileEntry (builtins.readDir dotfilesDir)
+    )
+  );
 }
