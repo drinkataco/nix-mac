395288a2d9feed4b196ad165f99cfdf351666ad6
J J Walwyn <git@jo.shw.al>
2026-03-14 22:08:31 +0000

    Use Nix git in TPM bootstrap

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -1,10 +1,10 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   home.activation.installTpm =
     lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       if [ ! -d "$HOME/.tmux/plugins/tpm/.git" ]; then
         mkdir -p "$HOME/.tmux/plugins"
-        git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+        "${pkgs.git}/bin/git" clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
       fi
     '';
 }
