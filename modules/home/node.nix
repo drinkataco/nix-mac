f20c846e34534b2b2b8b50760eb3f3ef8d80628a
J J Walwyn <git@jo.shw.al>
2026-04-11 11:45:57 +0100

    Add default fnm node version

--- modules/home/node.nix
+++ modules/home/node.nix
@@ -1,7 +1,25 @@
 { lib, pkgs, ... }:
+let
+  nodeVersions = [
+    "20"
+    "22"
+    "24"
+  ];
+in
 {
-  home.activation.installGlobalNodeTools =
+  home.activation.installFnmNodeVersions =
     lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      export PATH="${pkgs.fnm}/bin:$PATH"
+
+      ${lib.concatMapStringsSep "\n" (version: ''
+        fnm install ${version}
+      '') nodeVersions}
+
+      fnm default 24
+    '';
+
+  home.activation.installGlobalNodeTools =
+    lib.hm.dag.entryAfter [ "installFnmNodeVersions" ] ''
       export PNPM_HOME="$HOME/.local/share/pnpm"
       export PATH="$PNPM_HOME:${pkgs.pnpm}/bin:$PATH"
 
