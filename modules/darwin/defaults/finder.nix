7c296fe6d60b7c36a453e2e00fd60c0f183306cc
J J Walwyn <git@jo.shw.al>
2026-04-20 20:00:59 +0100

    Hide Finder favorite tags

--- modules/darwin/defaults/finder.nix
+++ modules/darwin/defaults/finder.nix
@@ -12,6 +12,9 @@
   };
 
   system.defaults.CustomUserPreferences."com.apple.finder" = {
+    # Favorite tags populate Finder's right-click shortcut menu. Keep this
+    # empty to remove the tag colours from file/folder context menus.
+    FavoriteTagNames = [ ];
     ShowRecentTags = false;
   };
 }
