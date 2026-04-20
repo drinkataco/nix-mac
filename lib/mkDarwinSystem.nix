c64272058975d1e4f224f1b0bced9b7f4a6950de
J J Walwyn <git@jo.shw.al>
2026-04-20 20:30:22 +0100

    Fix Darwin Home Manager module shape

--- lib/mkDarwinSystem.nix
+++ lib/mkDarwinSystem.nix
@@ -10,19 +10,21 @@ darwin.lib.darwinSystem {
     ({ config, lib, ... }: {
       options.features.gamesDir = lib.mkEnableOption "the user games application directory";
 
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.extraSpecialArgs = {
-        inherit inputs hostname system username;
-        inherit (config) features;
-      };
-      home-manager.users.${username} = { lib, ... }: {
-        imports = [
-          ../modules/home
-        ];
-        # Home Manager on Darwin can otherwise leave homeDirectory as null.
-        home.username = lib.mkForce username;
-        home.homeDirectory = lib.mkForce "/Users/${username}";
+      config = {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = {
+          inherit inputs hostname system username;
+          inherit (config) features;
+        };
+        home-manager.users.${username} = { lib, ... }: {
+          imports = [
+            ../modules/home
+          ];
+          # Home Manager on Darwin can otherwise leave homeDirectory as null.
+          home.username = lib.mkForce username;
+          home.homeDirectory = lib.mkForce "/Users/${username}";
+        };
       };
     })
     ../hosts/${hostname}
