61bb8258f0fff8b837914463dfa145b012d23091
J J Walwyn <git@jo.shw.al>
2026-05-08 09:48:55 +0100

    Revert cmdline hiding functionality

--- dotfiles/.config/nvim/lua/settings/options.lua
+++ dotfiles/.config/nvim/lua/settings/options.lua
@@ -19,9 +19,7 @@ opt.number = true
 opt.numberwidth = 2
 opt.mouse = "a"
 opt.mousemoveevent = true
--- Collapse the command line until Neovim needs it, while still allowing
--- command output to stay visible until the next editor action.
-opt.cmdheight = 0
+opt.cmdheight = 1
 opt.signcolumn = "yes"
 opt.splitbelow = true
 opt.splitright = true
