b449560f8bca4c0400e9cb645a33a77a9bae8935
J J Walwyn <git@jo.shw.al>
2026-05-21 14:50:40 +0100

    Add description placeholder to neogen tsdoc template

--- dotfiles/.config/nvim/lua/plugins/config/neogen.lua
+++ dotfiles/.config/nvim/lua/plugins/config/neogen.lua
@@ -1,12 +1,17 @@
 return function()
+  local i = require("neogen.types.template").item
+  local tsdoc = require("neogen.templates.tsdoc")
+  -- Insert description placeholder after the opening /** for funcs/classes with results
+  table.insert(tsdoc, 7, { nil, " * $1", { type = { "func", "class" } } })
+
   require("neogen").setup({
     snippet_engine = "luasnip",
     languages = {
       typescript = {
-        template = { annotation_convention = "tsdoc" },
+        template = { annotation_convention = "tsdoc", tsdoc = tsdoc },
       },
       typescriptreact = {
-        template = { annotation_convention = "tsdoc" },
+        template = { annotation_convention = "tsdoc", tsdoc = tsdoc },
       },
     },
   })
