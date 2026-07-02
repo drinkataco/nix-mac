a44599355fd534487fb2226aaa5adefe5bfedbdf
J J Walwyn <git@jo.shw.al>
2026-07-02 12:32:33 +0100

    Truncate prompt path to last component when longer than 40 chars

--- dotfiles/.zsh/prompt.zsh
+++ dotfiles/.zsh/prompt.zsh
@@ -4,9 +4,24 @@
 # PROMPT                 #
 ##########################
 
+# Show only the last path component when the rendered path exceeds this length.
+_PROMPT_PWD_MAX=40
+
+function _smart_pwd() {
+  local full
+  full=$(prompt-pwd)
+  if (( ${#full} > _PROMPT_PWD_MAX )); then
+    print -Pn '%1~'
+  else
+    print -rn "$full"
+  fi
+}
+
 # 1. Add Virtual ENV prompt
 # 2. Keep the gitster prompt shape, but use the preferred glyph for the leading prompt marker.
-PROMPT='${VIRTUAL_ENV_PROMPT:+%f(venv)%f }'"${PROMPT//➜/}"
+# 3. Replace prompt-pwd with _smart_pwd to truncate long paths.
+PROMPT='${VIRTUAL_ENV_PROMPT:+%f(venv)%f }'"${PROMPT//➜/}"
+PROMPT="${PROMPT//\$(prompt-pwd)/\$(_smart_pwd)}"
 
 # Show normal mode in the right prompt without replacing the existing git status.
 typeset -g ZVM_RPROMPT_BASE="${RPROMPT}"
